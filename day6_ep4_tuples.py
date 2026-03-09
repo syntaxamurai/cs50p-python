90e17587297d71ef369cdf468a8214fc10e34a17
Syntaxamurai <justinabwunza@gmail.com>
2026-03-09 19:15:34 +0300

    Working with tuples

---
+++ day6_ep4_tuples.py
@@ -0,0 +1,11 @@
+#Tuples are same as list, but immutable
+names = ('Justin', 'Mary', 'John', 'Alice', 'David', 'Grace', 'Daniel')
+updated_names = names
+
+print(names[:3])
+print(updated_names)
+
+# names[0] = "Abwunza"
+
+# print(names)
+# print(updated_names)
