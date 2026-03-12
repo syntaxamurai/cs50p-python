a71ac37480b7272a56795a6ec6f94dc6a985d4fc
Syntaxamurai <justinabwunza@gmail.com>
2026-03-12 15:10:22 +0300

    Returning booleans

--- lecture1.py
+++ lecture1.py
@@ -30,9 +30,26 @@ else:
 
 #Modulo
 
+'''
 x = int(input("What's x?: "))
 
 if x % 2 == 0:
     print("Even")
 else:
     print("Odd")
+'''
+
+def main():
+    x = int(input("What's x?: "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
