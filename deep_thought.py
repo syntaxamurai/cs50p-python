f9455c1a1e9be5de55dd3cb3248110fcf1ac7ee2
Syntaxamurai <justinabwunza@gmail.com>
2026-03-13 16:26:12 +0300

    deep_thought

---
+++ deep_thought.py
@@ -0,0 +1,10 @@
+'''
+In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
+'''
+
+deep = input("What is the Answer to the Great Question of Life, the Universe, and Everything?: ").lower().strip()
+
+if deep in ["42", "forty-two", "forty two"]:
+    print("Yes")
+else:
+    print("No")
